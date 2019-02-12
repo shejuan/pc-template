3ead3c6993dbe471477bc9ae953338650bea6b73
shejuan <1104875066@qq.com>
2019-02-12 09:34:13 +0800

    首页提交

--- src/pages/website/contact/contact.vue
+++ src/pages/website/contact/contact.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        联系人的内容
+        联系人的内容aasaaaa
     </div>
 </template>
 <style src="./contact.scss" lang="scss" scoped></style>
--- src/pages/website/shouye/shouye.vue
+++ src/pages/website/shouye/shouye.vue
@@ -1,6 +1,46 @@
 <template>
-    <div>
-        首页的内容
+    <div class="content">
+<div class="div1">
+    <img src="/static/images/36.png">
+</div>
+
+<div class="div2">
+    <div class="title">智能抄表收费功能
+        <ul>
+            <li>111</li>
+            <li>222</li>
+            <li>333</li>
+        </ul>
+    </div>
+</div>
+
+<div class="div3"></div>
+
+<div class="div4"></div>
     </div>
 </template>
+<style>
+.title {
+    border:1px solid gold;
+    height:100px;
+    font-size: 18px;
+}
+.content {
+    width: 100%;
+    height:2000px;
+}
+.div1 {
+    height:25%;
+    border:1px solid red;
+}
+.div1 img {
+    width:100%;
+    height:100%;
+}
+.div2 {
+    width:100%;
+    height: 25%;
+    border:1px solid green
+}
+</style>
 <style src="./shouye.scss" lang="scss" scoped></style>
